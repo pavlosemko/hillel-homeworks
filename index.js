10971bbe6a25ce963457e928f6731b5057cedd64
Pavlo <pavelsssemko@gmail.com>
2023-08-26 17:44:30 +0300

    finish hw

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 window.onload = function () {
-    class Table {
-        #table;
-        #createTable = () => document.createElement("table");
-        #createRow = () => document.createElement("tr");
-        #createCeil = () => document.createElement("td");
-        constructor() {
-            this.#table = this.#createTable();
-        }
-        genTableBodyByArray(array) {
-            for (const iterator of array) {
-                const row = this.#createRow();
-                for (const data of iterator) {
-                    const cell = this.#createCeil();
-                    cell.innerText = data;
-                    row.append(cell);
-                }
-                this.#table.append(row);
-            }
-        }
-        appendTo(element) {
-            element.append(this.#table);
-        }
-        get render() {
-            return this.#table;
-        }
-    }
+    // class Table {
+    //     #table;
+    //     #createTable = () => document.createElement("table");
+    //     #createRow = () => document.createElement("tr");
+    //     #createCeil = () => document.createElement("td");
+    //     constructor() {
+    //         this.#table = this.#createTable();
+    //     }
+    //     genTableBodyByArray(array) {
+    //         for (const iterator of array) {
+    //             const row = this.#createRow();
+    //             for (const data of iterator) {
+    //                 const cell = this.#createCeil();
+    //                 cell.innerText = data;
+    //                 row.append(cell);
+    //             }
+    //             this.#table.append(row);
+    //         }
+    //     }
+    //     appendTo(element) {
+    //         element.append(this.#table);
+    //     }
+    //     get render() {
+    //         return this.#table;
+    //     }
+    // }
+
+    // const createSequentialArray = (rows, cols) => {
+    //     const array = new Array(rows);
+    //     for (let i = 0; i < rows; i++) {
+    //         array[i] = new Array(cols);
+    //         for (let j = 0; j < cols; j++) {
+    //             array[i][j] = i * cols + j + 1;
+    //         }
+    //     }
+    //     return array;
+    // };
+
+    // const array = createSequentialArray(10, 10);
+
+    // const table = new Table();
+    // table.genTableBodyByArray(array);
+    // table.appendTo(document.body);
+
+
+
+    const createElByTagName = (tag) => document.createElement(tag);
+    const createCell = () => createElByTagName("td");
+    const createRow = () => createElByTagName("tr");
+    const createTable = () => createElByTagName("table");
 
     const createSequentialArray = (rows, cols) => {
         const array = new Array(rows);
@@ -36,10 +60,24 @@ window.onload = function () {
         }
         return array;
     };
+    const arr = createSequentialArray(10, 10);
 
-    const array = createSequentialArray(10, 10);
+    const fillTableByArray = (array) => {
 
-    const table = new Table();
-    table.genTableBodyByArray(array);
-    table.appendTo(document.body);
+        const table = createTable();
+
+        for (const iterator of array) {
+            const row = createRow();
+            for (const data of iterator) {
+                const cell = createCell();
+                cell.innerText = data;
+                row.append(cell);
+            }
+            table.append(row);
+        }
+        return table
+    };
+
+    
+    document.body.append(fillTableByArray(arr))
 };
